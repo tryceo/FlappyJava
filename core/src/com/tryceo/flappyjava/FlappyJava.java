98c8eb8d69304bd63d93ce63c4e1d197905c35b9
tryceo <tryceo@gmail.com>
2014-12-29 08:26:59 -0500

    Initial commit

---
+++ core/src/com/tryceo/flappyjava/FlappyJava.java
@@ -0,0 +1,15 @@
+package com.tryceo.flappyjava;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+
+public class FlappyJava extends Game {
+	SpriteBatch batch;
+
+	@Override
+	public void create() {
+		Gdx.app.log("FlappyJava", "created");
+
+	}
+}
---
+++ html/src/com/tryceo/flappyjava/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.tryceo.flappyjava.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.tryceo.flappyjava.FlappyJava;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new FlappyJava();
+        }
+}
